9a8836607fd9113cde2ac27d820b0522328356e3
Isaque Gomes <isaquegeovani29@gmail.com>
2026-02-05 23:50:16 -0300

    feat: adding user entity and resources

---
+++ src/main/java/com/devgomes/project/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.devgomes.project.resources;
+
+import com.devgomes.project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User (1L, "gomes", "gomes@gmail.com", "9199999999", "gomes232");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
