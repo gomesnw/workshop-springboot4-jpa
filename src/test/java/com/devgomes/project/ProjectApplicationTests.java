8925101131bf2c6931dbdff86eeead7538c8a1ac
Isaque Gomes <isaquegeovani29@gmail.com>
2026-02-05 23:32:14 -0300

    feat: initial commit with spring boot project structure

---
+++ src/test/java/com/devgomes/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgomes.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
