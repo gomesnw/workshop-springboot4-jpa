b53eacecab506866ff5f877984c4f035a4efc5a3
Isaque Gomes <isaquegeovani29@gmail.com>
2026-02-11 18:53:05 -0300

    feat: implementing exception handling for deleteUser method

--- src/main/java/com/devgomes/project/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devgomes/project/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devgomes.project.resources.exceptions;
 
+import com.devgomes.project.services.exceptions.DatabaseException;
 import com.devgomes.project.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,7 +17,24 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException exception, HttpServletRequest request){
         String error = "Resource not found.";
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError standardError = new StandardError(Instant.now(), status.value(), error, exception.getMessage(), request.getRequestURI());
+        StandardError standardError = new StandardError(Instant.now(),
+                status.value(),
+                error,
+                exception.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException exception, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError standardError = new StandardError(
+                Instant.now(),
+                status.value(),
+                error,
+                exception.getMessage(),
+                request.getRequestURI());
         return ResponseEntity.status(status).body(standardError);
     }
 }
--- src/main/java/com/devgomes/project/services/UserService.java
+++ src/main/java/com/devgomes/project/services/UserService.java
@@ -2,13 +2,17 @@ package com.devgomes.project.services;
 
 import com.devgomes.project.entities.User;
 import com.devgomes.project.repositories.UserRepository;
+import com.devgomes.project.services.exceptions.DatabaseException;
 import com.devgomes.project.services.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -28,8 +32,16 @@ public class UserService {
         return repository.save(user);
     }
 
-    public void deleteUser(Long id){
-        repository.deleteById(id);
+    public void deleteUser(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            log.error("Integrity violation by deleting user {}: {}", id, e.getMessage());
+            throw new DatabaseException("Can't delete an user that has pending orders.");
+        }
     }
 
     public User updateUser(Long id, User user){
